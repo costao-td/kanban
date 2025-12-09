41356d23afd6fb0787941c1802c791fb3904dd30
Arthur Schurhaus <arthurschur@icloud.com>
2025-12-09 11:02:36 -0300

    bumped to next16

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,14 +1,11 @@
+import next from "eslint-config-next";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import baseConfig, { restrictEnvAccess } from "@kan/eslint-config/base";
 import nextjsConfig from "@kan/eslint-config/nextjs";
 import reactConfig from "@kan/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: [".next/**"],
-  },
-  ...baseConfig,
-  ...reactConfig,
-  ...nextjsConfig,
-  ...restrictEnvAccess,
-];
+export default [...next, ...nextCoreWebVitals, ...nextTypescript, {
+  ignores: [".next/**"],
+}, ...baseConfig, ...reactConfig, ...nextjsConfig, ...restrictEnvAccess];
--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { env } from "next-runtime-env";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
     if (env("NEXT_PUBLIC_KAN_ENV") !== "cloud") {
       const loginUrl = new URL("/login", request.url);
